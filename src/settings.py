161997835e2a6edc23c51047353d87ae01509a38
Naman Tamrakar <namantam1@gmail.com>
2022-09-01 00:15:38 +0530

    Update to pipfile

--- src/settings.py
+++ src/settings.py
@@ -177,3 +177,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
